e804e93e662ff4a7e18d44c40e297bb7408987d5
IosifSoln <74780917+IosifSoln@users.noreply.github.com>
2021-01-13 13:06:45 +0300

    Homework2.4

---
+++ Window.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    private JTextField field;
+    private final JTextArea area;
+    private JButton button;
+
+    public Window() {
+        setTitle("chat");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(100, 100, 400, 400);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        area = new JTextArea();
+        area.setLineWrap(true);
+        JScrollPane scroll = new JScrollPane(area);
+        panel.add(scroll, BorderLayout.CENTER);
+
+        JPanel botoomJp = new JPanel();
+        botoomJp.setLayout(new BorderLayout());
+
+        addTextField();
+        botoomJp.add(field, BorderLayout.CENTER);
+
+        addButton();
+        botoomJp.add(button, BorderLayout.SOUTH);
+        panel.add(botoomJp, BorderLayout.SOUTH);
+        add(panel);
+
+        setVisible(true);
+        field.grabFocus();
+    }
+
+    private void sendMessage() {
+        if (!field.getText().isEmpty()) {
+            area.append(field.getText() + "\n");
+            field.setText(" ");
+            field.grabFocus();
+        }
+    }
+
+    private void addButton() {
+        button = new JButton("отправить");
+        button.addActionListener(e -> sendMessage());
+    }
+
+    private void addTextField() {
+        field = new JTextField();
+        field.addActionListener(e -> sendMessage());
+
+    }
+}
